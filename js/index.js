d27c6dfb67eaa5120a7bb1283c826691646ebd3a
vantruongac <vantruongac@gmail.com>
2023-12-23 09:53:28 +0700

    header

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// JavaScript ES6
+const toggleButton = document.getElementById('toggleButton');
+const navbar = document.getElementById('navbar');
+
+toggleButton.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+});
+
+
+
+
+// JavaScript ES6
+// const myButton = document.getElementById('myButton');
+
+// myButton.addEventListener('click', () => {
+//   console.log('Button clicked!');
+//   // Thêm mã lệnh xử lý bạn muốn thực hiện khi nút được click ở đây
+// });
